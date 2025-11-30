b602de338525ab425af2bd9c2844c0b5e084928e
cristhian <cristhianfernandolp@gmail.com>
2025-11-30 20:41:12 -0300

    chore: add base Comment class

--- src/domain/forum/enterprise/entities/answer-comment.ts
+++ src/domain/forum/enterprise/entities/answer-comment.ts
@@ -1,41 +1,15 @@
 import type { UniqueEntityID } from "@/core/entities/unique-entity-id.js";
-import { Entity } from "@/core/entities/entity.js";
 import type { Optional } from "@/@types/optional.js";
+import { Comment, type CommentProps } from "./comment";
 
-export interface AnswerCommentProps {
-  authorId: UniqueEntityID;
+export interface AnswerCommentProps extends CommentProps {
   answerId: UniqueEntityID;
-  content: string;
-  createdAt: Date;
-  updatedAt?: Date;
 }
 
-export class AnswerComment extends Entity<AnswerCommentProps> {
-  get authorId() {
-    return this.props.authorId;
-  }
+export class AnswerComment extends Comment<AnswerCommentProps> {
   get answerId() {
     return this.props.answerId;
   }
-  get createdAt() {
-    return this.props.createdAt;
-  }
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-  get content() {
-    return this.props.content;
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date();
-  }
-
-  set content(content: string) {
-    this.props.content = content;
-    this.touch();
-  }
-
   static create(
     props: Optional<AnswerCommentProps, "createdAt">,
     id?: UniqueEntityID,
--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,39 +1,14 @@
 import type { UniqueEntityID } from "@/core/entities/unique-entity-id.js";
-import { Entity } from "@/core/entities/entity.js";
 import type { Optional } from "@/@types/optional.js";
+import { Comment, type CommentProps } from "./comment";
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID;
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID;
-  content: string;
-  createdAt: Date;
-  updatedAt?: Date;
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId;
-  }
+export class QuestionComment extends Comment<QuestionCommentProps> {
   get questionId() {
     return this.props.questionId;
-  }
-  get createdAt() {
-    return this.props.createdAt;
-  }
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-  get content() {
-    return this.props.content;
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date();
-  }
-
-  set content(content: string) {
-    this.props.content = content;
-    this.touch();
   }
 
   static create(
