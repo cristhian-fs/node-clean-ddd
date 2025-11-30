6fb9c40f5e46e0f298bd3ea89a1384b21afbf0e5
cristhian <cristhianfernandolp@gmail.com>
2025-11-30 20:17:59 -0300

    feat: add new domain entitites

---
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -0,0 +1,53 @@
+import type { UniqueEntityID } from "@/core/entities/unique-entity-id.js";
+import { Entity } from "@/core/entities/entity.js";
+import type { Optional } from "@/@types/optional.js";
+
+export interface QuestionCommentProps {
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
+  content: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class QuestionComment extends Entity<QuestionCommentProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+  get questionId() {
+    return this.props.questionId;
+  }
+  get createdAt() {
+    return this.props.createdAt;
+  }
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+  get content() {
+    return this.props.content;
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  static create(
+    props: Optional<QuestionCommentProps, "createdAt">,
+    id?: UniqueEntityID,
+  ) {
+    const answer = new QuestionComment(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    );
+
+    return answer;
+  }
+}
